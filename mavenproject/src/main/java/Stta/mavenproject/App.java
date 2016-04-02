826a6880cf591c664fca49346c06646f431063da
VenkateshLagadapati <venkatesh.lagadapati@gmail.com>
2016-04-02 11:39:55 +0530

    second commit

--- mavenproject/src/main/java/Stta/mavenproject/App.java
+++ mavenproject/src/main/java/Stta/mavenproject/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	//second commit
+    	//second commit j
         System.out.println( "Hello World!" );
         //first commit
     }

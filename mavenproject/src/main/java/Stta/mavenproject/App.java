edc68d90d0ab662452e71a76237819793e1b7722
VenkateshLagadapati <venkatesh.lagadapati@gmail.com>
2016-04-02 11:26:42 +0530

    first commit

---
+++ mavenproject/src/main/java/Stta/mavenproject/App.java
@@ -0,0 +1,13 @@
+package Stta.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
